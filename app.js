4f0ab858624e16aed9b344dc2ec658fcc76e442f
tiemfah <sivanat.sub@gmail.com>
2019-08-19 09:59:41 +0700

    will add them later

--- app.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express()
-const port = 3000
-
-app.use(express.static(__dirname + '/public'));
-
-app.get('/', (req, res) => res.sendFile(__dirname+'/index.html'))
-
-app.listen(port, () => console.log(`app listening on port ${port}!`))
