c1f0a0095c18f1602bbf513dc3985846851e7ae9
Sivanat Subpaisarn <sivanats@Sivanats-MacBook-Pro.local>
2019-10-12 16:27:13 +0700

    remove TODO

--- pages/watch.js
+++ pages/watch.js
@@ -4,16 +4,16 @@ import '../public/css/watch.css'
 const watch = () => (
     <Layout>
         <div>
-            <div class="spacing"></div>
+            <div className="spacing"></div>
 
-            <div class="secTitle">
+            <div className="secTitle">
                 <h1 className="fw-300">PAST SPEAKERS</h1>
-                <hr class="separator separator--line" />
+                <hr className="separator separator--line" />
             </div>
 
-            <div class="lazy thisyear" id="year18">
+            <div className="lazy thisyear" id="year18">
                 <h1 className="fw-100">Out of Norm</h1>
-                <div class="lazy slider-wrapper">
+                <div className="lazy slider-wrapper">
                     <h4>201</h4>
                     <h4>
                         <span>
@@ -25,94 +25,94 @@ const watch = () => (
             </div>
 
             <section id="yr18">
-                <div class="row mx-auto">
-                    <div class="col-md-6 col-lg-4 mx-auto ytbuild">
-                        <div class="vdo-border text-center mb-4 p-3">
-                            <div class="yt-thumbnail" id="1"></div>
-                            <p class="speakerTopic">ความรัก หรือ ความฝัน</p>
-                            <p class="speakerName">รัณนภันต์ ยั่งยืนพูนชัย</p>
+                <div className="row mx-auto">
+                    <div className="col-md-6 col-lg-4 mx-auto ytbuild">
+                        <div className="vdo-border text-center mb-4 p-3">
+                            <div className="yt-thumbnail" id="1"></div>
+                            <p className="speakerTopic">ความรัก หรือ ความฝัน</p>
+                            <p className="speakerName">รัณนภันต์ ยั่งยืนพูนชัย</p>
                         </div>
                     </div>
 
-                    <div class="col-md-6 col-lg-4 mx-auto ytbuild">
-                        <div class="vdo-border text-center mb-4 p-3">
-                            <div class="yt-thumbnail" id="2"></div>
-                            <p class="speakerTopic">let me glow</p>
-                            <p class="speakerName">ณัฏฐนันท์ สนุ่นรัตน์</p>
+                    <div className="col-md-6 col-lg-4 mx-auto ytbuild">
+                        <div className="vdo-border text-center mb-4 p-3">
+                            <div className="yt-thumbnail" id="2"></div>
+                            <p className="speakerTopic">let me glow</p>
+                            <p className="speakerName">ณัฏฐนันท์ สนุ่นรัตน์</p>
                         </div>
                     </div>
 
-                    <div class="col-md-6 col-lg-4 mx-auto ytbuild">
-                        <div class="vdo-border text-center mb-4 p-3">
-                            <div class="yt-thumbnail" id="3"></div>
-                            <p class="speakerTopic">Limitless</p>
-                            <p class="speakerName">ตรีชฎา เพชรรัตน์</p>
+                    <div className="col-md-6 col-lg-4 mx-auto ytbuild">
+                        <div className="vdo-border text-center mb-4 p-3">
+                            <div className="yt-thumbnail" id="3"></div>
+                            <p className="speakerTopic">Limitless</p>
+                            <p className="speakerName">ตรีชฎา เพชรรัตน์</p>
                         </div>
                     </div>
 
-                    <div class="col-md-6 col-lg-4 mx-auto ytbuild">
-                        <div class="vdo-border text-center mb-4 p-3">
-                            <div class="yt-thumbnail" id="4"></div>
-                            <p class="speakerTopic">who am I?</p>
-                            <p class="speakerName">ณัฐวุฒิ พึงเจริญพงศ์</p>
+                    <div className="col-md-6 col-lg-4 mx-auto ytbuild">
+                        <div className="vdo-border text-center mb-4 p-3">
+                            <div className="yt-thumbnail" id="4"></div>
+                            <p className="speakerTopic">who am I?</p>
+                            <p className="speakerName">ณัฐวุฒิ พึงเจริญพงศ์</p>
                         </div>
                     </div>
 
-                    <div class="col-md-6 col-lg-4 mx-auto ytbuild">
-                        <div class="vdo-border text-center mb-4 p-3">
-                            <div class="yt-thumbnail" id="5"></div>
-                            <p class="speakerTopic">defeat your limit</p>
-                            <p class="speakerName">อริสรา ธนาปกิจ</p>
+                    <div className="col-md-6 col-lg-4 mx-auto ytbuild">
+                        <div className="vdo-border text-center mb-4 p-3">
+                            <div className="yt-thumbnail" id="5"></div>
+                            <p className="speakerTopic">defeat your limit</p>
+                            <p className="speakerName">อริสรา ธนาปกิจ</p>
                         </div>
                     </div>
 
-                    <div class="col-md-6 col-lg-4 mx-auto ytbuild">
-                        <div class="vdo-border text-center mb-4 p-3">
-                            <div class="yt-thumbnail" id="6"></div>
-                            <p class="speakerTopic">Head hand heart</p>
-                            <p class="speakerName">ปรารี กิตติดำเกิง</p>
+                    <div className="col-md-6 col-lg-4 mx-auto ytbuild">
+                        <div className="vdo-border text-center mb-4 p-3">
+                            <div className="yt-thumbnail" id="6"></div>
+                            <p className="speakerTopic">Head hand heart</p>
+                            <p className="speakerName">ปรารี กิตติดำเกิง</p>
                         </div>
                     </div>
 
-                    <div class="col-md-6 col-lg-4 mx-auto ytbuild">
-                        <div class="vdo-border text-center mb-4 p-3">
-                            <div class="yt-thumbnail" id="7"></div>
-                            <p class="speakerTopic">drift for alive</p>
-                            <p class="speakerName">คริสตี้หลุยส์ นามวงศ์</p>
+                    <div className="col-md-6 col-lg-4 mx-auto ytbuild">
+                        <div className="vdo-border text-center mb-4 p-3">
+                            <div className="yt-thumbnail" id="7"></div>
+                            <p className="speakerTopic">drift for alive</p>
+                            <p className="speakerName">คริสตี้หลุยส์ นามวงศ์</p>
                         </div>
                     </div>
 
-                    <div class="col-md-6 col-lg-4 mx-auto ytbuild">
-                        <div class="vdo-border text-center mb-4 p-3">
-                            <div class="yt-thumbnail" id="8"></div>
-                            <p class="speakerTopic">Can u hear your monster?</p>
-                            <p class="speakerName">ชานนท์ ยอดหงษ์</p>
+                    <div className="col-md-6 col-lg-4 mx-auto ytbuild">
+                        <div className="vdo-border text-center mb-4 p-3">
+                            <div className="yt-thumbnail" id="8"></div>
+                            <p className="speakerTopic">Can u hear your monster?</p>
+                            <p className="speakerName">ชานนท์ ยอดหงษ์</p>
                         </div>
                     </div>
 
-                    <div class="col-md-6 col-lg-4 mx-auto ytbuild">
-                        <div class="vdo-border text-center mb-4 p-3">
-                            <div class="yt-thumbnail" id="9"></div>
-                            <p class="speakerTopic">Beyond lie the solutions.</p>
-                            <p class="speakerName">จิตร์ทัศน์ ฝักเจริญผล</p>
+                    <div className="col-md-6 col-lg-4 mx-auto ytbuild">
+                        <div className="vdo-border text-center mb-4 p-3">
+                            <div className="yt-thumbnail" id="9"></div>
+                            <p className="speakerTopic">Beyond lie the solutions.</p>
+                            <p className="speakerName">จิตร์ทัศน์ ฝักเจริญผล</p>
                         </div>
                     </div>
 
-                    <div class="col-md-6 col-lg-4 mx-auto ytbuild">
-                        <div class="vdo-border text-center mb-4 p-3">
-                            <div class="yt-thumbnail" id="10"></div>
-                            <p class="speakerTopic">write your own happy ending</p>
-                            <p class="speakerName">ภาณุมาศ ทองธนากุล</p>
+                    <div className="col-md-6 col-lg-4 mx-auto ytbuild">
+                        <div className="vdo-border text-center mb-4 p-3">
+                            <div className="yt-thumbnail" id="10"></div>
+                            <p className="speakerTopic">write your own happy ending</p>
+                            <p className="speakerName">ภาณุมาศ ทองธนากุล</p>
                         </div>
                     </div>
                 </div>
 
-                <hr class="separator separator--line" />
+                <hr className="separator separator--line" />
             </section>
 
-            <div class="lazy thisyear" id="year17">
+            <div className="lazy thisyear" id="year17">
                 <h1 className="fw-100">Take a change, Make a chance</h1>
-                <div class="lazy slider-wrapper">
+                <div className="lazy slider-wrapper">
                     <h4>201</h4>
                     <h4>
                         <span>
@@ -125,84 +125,84 @@ const watch = () => (
             </div>
 
             <section id="yr17">
-                <div class="row mx-auto">
-                    <div class="col-md-6 col-lg-4 mx-auto ytbuild">
-                        <div class="vdo-border text-center mb-4 p-3">
-                            <div class="yt-thumbnail" id="1"></div>
-                            <p class="speakerTopic">ฝันเด็กไทย ใครกำหนด</p>
-                            <p class="speakerName">อรพิมพ์ เหลืองอ่อน</p>
+                <div className="row mx-auto">
+                    <div className="col-md-6 col-lg-4 mx-auto ytbuild">
+                        <div className="vdo-border text-center mb-4 p-3">
+                            <div className="yt-thumbnail" id="1"></div>
+                            <p className="speakerTopic">ฝันเด็กไทย ใครกำหนด</p>
+                            <p className="speakerName">อรพิมพ์ เหลืองอ่อน</p>
                         </div>
                     </div>
 
-                    <div class="col-md-6 col-lg-4 mx-auto ytbuild">
-                        <div class="vdo-border text-center mb-4 p-3">
-                            <div class="yt-thumbnail" id="2"></div>
-                            <p class="speakerTopic">สมองกลสอนคน</p>
-                            <p class="speakerName">ดร.กาญจนพันธุ์ สุขวิชชัย</p>
+                    <div className="col-md-6 col-lg-4 mx-auto ytbuild">
+                        <div className="vdo-border text-center mb-4 p-3">
+                            <div className="yt-thumbnail" id="2"></div>
+                            <p className="speakerTopic">สมองกลสอนคน</p>
+                            <p className="speakerName">ดร.กาญจนพันธุ์ สุขวิชชัย</p>
                         </div>
                     </div>
 
-                    <div class="col-md-6 col-lg-4 mx-auto ytbuild">
-                        <div class="vdo-border text-center mb-4 p-3">
-                            <div class="yt-thumbnail" id="3"></div>
-                            <p class="speakerTopic">สื่อการเรียน...เปลี่ยนอนาคต</p>
-                            <p class="speakerName">ฐิติวุฒิ นันทิภาคย์หิรัญ</p>
+                    <div className="col-md-6 col-lg-4 mx-auto ytbuild">
+                        <div className="vdo-border text-center mb-4 p-3">
+                            <div className="yt-thumbnail" id="3"></div>
+                            <p className="speakerTopic">สื่อการเรียน...เปลี่ยนอนาคต</p>
+                            <p className="speakerName">ฐิติวุฒิ นันทิภาคย์หิรัญ</p>
                         </div>
                     </div>
 
-                    <div class="col-md-6 col-lg-4 mx-auto ytbuild">
-                        <div class="vdo-border text-center mb-4 p-3">
-                            <div class="yt-thumbnail" id="4"></div>
-                            <p class="speakerTopic">หยุดตีตรา เพื่อเดินต่อ</p>
-                            <p class="speakerName">กิตติภณ ดิษฐบรรจง</p>
+                    <div className="col-md-6 col-lg-4 mx-auto ytbuild">
+                        <div className="vdo-border text-center mb-4 p-3">
+                            <div className="yt-thumbnail" id="4"></div>
+                            <p className="speakerTopic">หยุดตีตรา เพื่อเดินต่อ</p>
+                            <p className="speakerName">กิตติภณ ดิษฐบรรจง</p>
                         </div>
                     </div>
 
-                    <div class="col-md-6 col-lg-4 mx-auto ytbuild">
-                        <div class="vdo-border text-center mb-4 p-3">
-                            <div class="yt-thumbnail" id="5"></div>
-                            <p class="speakerTopic">เริ่มเมื่อยังไม่พร้อม</p>
-                            <p class="speakerName">พงศธร ธนบดีภัทร</p>
+                    <div className="col-md-6 col-lg-4 mx-auto ytbuild">
+                        <div className="vdo-border text-center mb-4 p-3">
+                            <div className="yt-thumbnail" id="5"></div>
+                            <p className="speakerTopic">เริ่มเมื่อยังไม่พร้อม</p>
+                            <p className="speakerName">พงศธร ธนบดีภัทร</p>
                         </div>
                     </div>
 
-                    <div class="col-md-6 col-lg-4 mx-auto ytbuild">
-                        <div class="vdo-border text-center mb-4 p-3">
-                            <div class="yt-thumbnail" id="6"></div>
-                            <p class="speakerTopic">พลิกชีวิตเกษตรกรไทย ด้วยไฟในใจคุณ</p>
-                            <p class="speakerName">พงษ์ลดา พะเนียงเวทย์</p>
+                    <div className="col-md-6 col-lg-4 mx-auto ytbuild">
+                        <div className="vdo-border text-center mb-4 p-3">
+                            <div className="yt-thumbnail" id="6"></div>
+                            <p className="speakerTopic">พลิกชีวิตเกษตรกรไทย ด้วยไฟในใจคุณ</p>
+                            <p className="speakerName">พงษ์ลดา พะเนียงเวทย์</p>
                         </div>
                     </div>
 
-                    <div class="col-md-6 col-lg-4 mx-auto ytbuild">
-                        <div class="vdo-border text-center mb-4 p-3">
-                            <div class="yt-thumbnail" id="7"></div>
-                            <p class="speakerTopic">เกลียดวันจันทร์?</p>
-                            <p class="speakerName">รวิศ หาญอุตสาหะ</p>
+                    <div className="col-md-6 col-lg-4 mx-auto ytbuild">
+                        <div className="vdo-border text-center mb-4 p-3">
+                            <div className="yt-thumbnail" id="7"></div>
+                            <p className="speakerTopic">เกลียดวันจันทร์?</p>
+                            <p className="speakerName">รวิศ หาญอุตสาหะ</p>
                         </div>
                     </div>
 
-                    <div class="col-md-6 col-lg-4 mx-auto ytbuild">
-                        <div class="vdo-border text-center mb-4 p-3">
-                            <div class="yt-thumbnail" id="8"></div>
-                            <p class="speakerTopic">ปลูกบ้านจากต้นหญ้า</p>
-                            <p class="speakerName">ธนพัฒน์ บุญสนาน</p>
+                    <div className="col-md-6 col-lg-4 mx-auto ytbuild">
+                        <div className="vdo-border text-center mb-4 p-3">
+                            <div className="yt-thumbnail" id="8"></div>
+                            <p className="speakerTopic">ปลูกบ้านจากต้นหญ้า</p>
+                            <p className="speakerName">ธนพัฒน์ บุญสนาน</p>
                         </div>
                     </div>
 
-                    <div class="col-md-6 col-lg-4 mx-auto ytbuild">
-                        <div class="vdo-border text-center mb-4 p-3">
-                            <div class="yt-thumbnail" id="9"></div>
-                            <p class="speakerTopic">Secrets to discovering your true passion and happiness in life.</p>
-                            <p class="speakerName">ดาริน สุทธพงศ์</p>
+                    <div className="col-md-6 col-lg-4 mx-auto ytbuild">
+                        <div className="vdo-border text-center mb-4 p-3">
+                            <div className="yt-thumbnail" id="9"></div>
+                            <p className="speakerTopic">Secrets to discovering your true passion and happiness in life.</p>
+                            <p className="speakerName">ดาริน สุทธพงศ์</p>
                         </div>
                     </div>
 
-                    <div class="col-md-6 col-lg-4 mx-auto ytbuild">
-                        <div class="vdo-border text-center mb-4 p-3">
-                            <div class="yt-thumbnail" id="10"></div>
-                            <p class="speakerTopic">เปลี่ยนกาย เปลี่ยนเสียง เปลี่ยนใจ เพื่อนิสัยใหม่ที่คุณต้องการ</p>
-                            <p class="speakerName">รสสุคนธ์ กองเกตุ</p>
+                    <div className="col-md-6 col-lg-4 mx-auto ytbuild">
+                        <div className="vdo-border text-center mb-4 p-3">
+                            <div className="yt-thumbnail" id="10"></div>
+                            <p className="speakerTopic">เปลี่ยนกาย เปลี่ยนเสียง เปลี่ยนใจ เพื่อนิสัยใหม่ที่คุณต้องการ</p>
+                            <p className="speakerName">รสสุคนธ์ กองเกตุ</p>
                         </div>
                     </div>
                 </div>
