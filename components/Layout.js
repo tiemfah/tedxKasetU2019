9abc44b4b526aaa30cf3f090e77de112e9cdf0fe
Chananchida F <chida.fua@gmail.com>
2019-10-12 19:43:54 +0700

    Still have prob w/ DOM in Next.js

--- components/Layout.js
+++ components/Layout.js
@@ -10,6 +10,8 @@ const Layout = (props) => (
             <title>TEDxKasetsartU</title>
             <link rel="shortcut icon" href="img/favicon.png" type="image/x-icon" />
             <script src="https://kit.fontawesome.com/21af1254cb.js"></script>
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+                integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"></link>
         </Head>
         <NavBar />
         {props.children}
