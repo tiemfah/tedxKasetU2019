add35cdc55d168fed91996f2d624097ce806823c
sidtipat.k <s.kietchai@outlook.com>
2019-10-13 12:25:11 +0700

    link contact

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,4 +1,4 @@
-$('body').delegate('.c-faq', 'click', function(){
-    $('.c-faq').removeClass('c-faq--active');
-    $(this).addClass('c-faq--active');
-  });
+$("body").delegate(".c-faq", "click", function() {
+  $(".c-faq").removeClass("c-faq--active");
+  $(this).addClass("c-faq--active");
+});
