64ee5188ded70d49d28c79fcb53de1dc1690809d
Chananchida F <chida.fua@gmail.com>
2019-10-14 18:48:05 +0700

    just setup

---
+++ pages/partner.js
@@ -0,0 +1,10 @@
+import Layout from '../components/Layout';
+import '../public/css/partner.css';
+
+const partner = () => (
+    <Layout>
+        <div>Partner</div>
+    </Layout>
+);
+
+export default partner;
