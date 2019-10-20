395c71063f4b6dbb7cb5893bd40e93f698e0b270
Thun <thun@Thuns-MacBook-Pro.local>
2019-10-20 22:39:49 +0700

    add sponser pic

--- pages/partner.js
+++ pages/partner.js
@@ -1,9 +1,21 @@
 import Layout from '../components/Layout';
+import AisImage from '../public/img/sponser/AIS.png'
+import ElecImage from '../public/img/sponser/elec.png'
+import EngImage from '../public/img/sponser/eng-ku.png'
+import OldEngImage from '../public/img/sponser/old-eng.png'
+import OsotImage from '../public/img/sponser/osot.png'
+
 import '../public/css/partner.css';
 
 const partner = () => (
     <Layout>
-        <div>Partner</div>
+        <div className="block-sponser">
+            <img src={AisImage} alt='partner' className="sponser"/>
+            <img src={ElecImage} alt='partner' className="sponser"/>
+            <img src={EngImage} alt='partner' className="sponser"/>
+            <img src={OldEngImage} alt='partner' className="sponser"/>
+            <img src={OsotImage} alt='partner' className="sponser"/>
+        </div>
     </Layout>
 );
 
