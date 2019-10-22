f95bc64d7b8a97f32c9fe5c8263c1c3a219cd3a3
sidtipat.k <s.kietchai@outlook.com>
2019-10-22 21:52:39 +0700

    add contact to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
-const withImage = require('next-images');
-const withCSS = require('@zeit/next-css')
-module.exports = withCSS(withImage({
-    exportPathMap: function () {
-        return {
-            '/': { page: '/' },
-            '/watch': { page: '/watch' },
-            '/participate':{page:'/participate'},
-            '/speaker':{page:'/speaker'},
-        };
+const withImage = require("next-images");
+const withCSS = require("@zeit/next-css");
+module.exports = withCSS(
+  withImage({
+    exportPathMap: function() {
+      return {
+        "/": { page: "/" },
+        "/watch": { page: "/watch" },
+        "/participate": { page: "/participate" },
+        "/speaker": { page: "/speaker" },
+        "/contact": { page: "/contact" }
+      };
     }
-}));
+  })
+);
