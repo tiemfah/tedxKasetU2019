26ae28d271442b79b796b11a717c7ea26ef08d5d
Chananchida F <chida.fua@gmail.com>
2019-10-27 23:11:05 +0700

    Add accordion on partner page

--- pages/partner.js
+++ pages/partner.js
@@ -6,9 +6,10 @@ import OldEngImage from '../public/img/sponser/old-eng.png'
 import OsotImage from '../public/img/sponser/osot.png'
 
 import '../public/css/partner.css';
+import { Card, Accordion } from 'react-bootstrap';
 
 const partner = () => (
-    <Layout>
+    <Layout isWhiteNav={true}>
         <div className="spacing"></div>
 
         {/* Page title */}
@@ -17,14 +18,52 @@ const partner = () => (
             <hr className="separate separate--line" />
         </div>
 
-        <div className="block-sponser">
-            <img src={AisImage} alt='partner' className="sponser" />
-            <img src={ElecImage} alt='partner' className="sponser" />
-            <img src={EngImage} alt='partner' className="sponser" />
-            <img src={OldEngImage} alt='partner' className="sponser" />
-            <img src={OsotImage} alt='partner' className="sponser" />
-        </div>
-    </Layout>
+        <Accordion className="col-lg-10 mx-auto">
+            <Card className="sponser-dd text-center mx-auto">
+                <Accordion.Toggle as={Card.Header} eventKey="0">
+                    <h5 className="sponser-year">2019 SPONSERS</h5>
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey="0">
+                    <Card.Body className="col-lg-8 mx-auto">
+                        <a href="https://www.pea.co.th/" target="blank"><img src={ElecImage} alt='partner' className="sponser" /></a>
+                        <a href="https://www.eng.ku.ac.th/" target="blank"><img src={EngImage} alt='partner' className="sponser" /></a>
+                        <a href="http://dongtanengineer.com/" target="blank"><img src={OldEngImage} alt='partner' className="sponser" /></a>
+                        <a href="http://www.ais.co.th/" target="blank"><img src={AisImage} alt='partner' className="sponser" /></a>
+                        <a href="https://www.osotspa.com/new/th/home/" target="blank"><img src={OsotImage} alt='partner' className="sponser" /></a>
+                    </Card.Body>
+                </Accordion.Collapse>
+            </Card>
+
+            <Card className="sponser-dd text-center mx-auto">
+                <Accordion.Toggle as={Card.Header} eventKey="1">
+                    <h5 className="sponser-year">2018 SPONSERS</h5>
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey="1">
+                    <Card.Body className="col-lg-8 mx-auto">
+                        <a href="https://www.pea.co.th/" target="blank"><img src={ElecImage} alt='partner' className="sponser" /></a>
+                        <a href="https://www.eng.ku.ac.th/" target="blank"><img src={EngImage} alt='partner' className="sponser" /></a>
+                        <a href="http://dongtanengineer.com/" target="blank"><img src={OldEngImage} alt='partner' className="sponser" /></a>
+                        <a href="http://www.ais.co.th/" target="blank"><img src={AisImage} alt='partner' className="sponser" /></a>
+                        <a href="https://www.osotspa.com/new/th/home/" target="blank"><img src={OsotImage} alt='partner' className="sponser" /></a>
+                    </Card.Body>
+                </Accordion.Collapse>
+            </Card>
+
+            <Card className="sponser-dd text-center mx-auto">
+                <Accordion.Toggle as={Card.Header} eventKey="2">
+                    <h5 className="sponser-year">2017 SPONSERS</h5>
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey="2">
+                    <Card.Body className="col-lg-8 mx-auto">
+                        <a href="https://www.eng.ku.ac.th/" target="blank"><img src={EngImage} alt='partner' className="sponser" /></a>
+                        <a href="http://dongtanengineer.com/" target="blank"><img src={OldEngImage} alt='partner' className="sponser" /></a>
+                    </Card.Body>
+                </Accordion.Collapse>
+            </Card>
+        </Accordion>
+
+        <div className="spacing"></div>
+    </Layout >
 );
 
 export default partner;
